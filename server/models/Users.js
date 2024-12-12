77854a4d7bcb38a42f66a3557e05c9dcb6cdcc9b
Ranjeet Singh Tharu <67905362+Ranjeetprgrmr@users.noreply.github.com>
2024-12-12 18:20:38 +0545

    Update Users.js

--- server/models/Users.js
+++ server/models/Users.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
     name: {
@@ -17,4 +17,5 @@ const UserSchema = new mongoose.Schema({
 });
 
 const UserModel = mongoose.model("users", UserSchema)
+
 export default UserModel
