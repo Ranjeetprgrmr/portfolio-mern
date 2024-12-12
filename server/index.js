ccdda79ccca30a890dcb7e8bd1b66b597283e719
Ranjeet Singh Tharu <67905362+Ranjeetprgrmr@users.noreply.github.com>
2024-12-12 17:31:41 +0545

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const UserModel = require("./models/Users");
+const UserModel = require("./models/users");
 
 //dotenv configuration
 dotenv.config();
