7f568897242839a533bf0b96aace9b9d81463236
Ranjeet Singh Tharu <67905362+Ranjeetprgrmr@users.noreply.github.com>
2024-12-12 17:51:04 +0545

    Update index.js

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const UserModel = require("./models/users");
+const UserModel = require("./models/Users.js");
 const PORT = process.env.PORT || 5000
 
 //dotenv configuration
