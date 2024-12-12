8968be4e64db40fbaec4c641724ee79e1a5c5dc9
Ranjeet Singh Tharu <67905362+Ranjeetprgrmr@users.noreply.github.com>
2024-12-12 18:17:48 +0545

    Update Users.js

--- models/Users.js
+++ models/Users.js
@@ -1,1 +1,20 @@
+const mongoose = require('mongoose');
 
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    message: {
+        type: String,
+        required: true
+    },
+
+});
+
+const UserModel = mongoose.model("users", UserSchema)
+export default UserModel
